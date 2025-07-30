65994d0efeede51d5bbda2fa8b9bfeb33d2748cc
nirajan124 <nirajanbhattarai20@gmail.com>
2025-07-30 23:04:53 +0545

    Auth change

--- web_api_backend-main/dairy_go_backend/middleware/auth.js
+++ web_api_backend-main/dairy_go_backend/middleware/auth.js
@@ -42,14 +42,3 @@ exports.protect = asyncHandler(async (req, res, next) => {
     }
 });
 
-// Role-Based Authorization Middleware
-exports.authorize = (...roles) => {
-    return (req, res, next) => {
-        if (!req.user || !roles.includes(req.user.role)) {
-            return res.status(403).json({
-                message: `User role '${req.user.role}' is not authorized to access this route`,
-            });
-        }
-        next();
-    };
-};
