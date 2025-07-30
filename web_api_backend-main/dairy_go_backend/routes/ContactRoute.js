22d0c6392a0403de0542f031776e62586585e31e
nirajan124 <nirajanbhattarai20@gmail.com>
2025-07-30 23:36:20 +0545

    edit contact

--- web_api_backend-main/dairy_go_backend/routes/ContactRoute.js
+++ web_api_backend-main/dairy_go_backend/routes/ContactRoute.js
@@ -3,11 +3,6 @@ const router = express.Router();
 const { saveMessage, getAllMessages, markAsRead } = require("../controllers/ContactController");
 const { protect, authorize } = require("../middleware/auth");
 
-// Public: Save a contact message
-router.post("/", saveMessage);
-// Admin only: Get all contact messages
-router.get("/", protect, authorize("admin"), getAllMessages);
-// Admin only: Mark a message as read
-router.patch("/:id/read", protect, authorize("admin"), markAsRead);
+
 
 module.exports = router; 
