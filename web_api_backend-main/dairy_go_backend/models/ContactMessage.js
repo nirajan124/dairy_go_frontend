a95e69cb72658f31d364415e830f39e63bf32365
nirajan124 <nirajanbhattarai20@gmail.com>
2025-07-30 23:39:29 +0545

    edit contact message

--- web_api_backend-main/dairy_go_backend/models/ContactMessage.js
+++ web_api_backend-main/dairy_go_backend/models/ContactMessage.js
@@ -4,9 +4,7 @@ const contactMessageSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String },
-  subject: { type: String },
-  message: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  
   read: { type: Boolean, default: false }
 });
 
